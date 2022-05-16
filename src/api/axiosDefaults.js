403e1c9e5b5f54757b76365d84aa63777142bcc8
Alaa Hijazi <alaa.higazy@hotmail.com>
2022-05-16 13:01:01 +0000

    create post form compononet and connected to my api

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
 axios.defaults.baseURL = "https://drf-api-dj.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
